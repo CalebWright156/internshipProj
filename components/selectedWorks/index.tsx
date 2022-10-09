3471da7fc80db3a43c2c784461e4c9581a1616bc
Caleb Wright <calebwright156@gmail.com>
2022-10-10 02:09:37 +1300

    began last bits

--- components/selectedWorks/index.tsx
+++ components/selectedWorks/index.tsx
@@ -6,9 +6,15 @@ import Image1 from '../../public/assets/3.png'
 import Image2 from '../../public/assets/4.png'
 import Image3 from '../../public/assets/5.png'
 import { LeftSideContent, RightSideContent} from '../styles/SelectedWorksStyles'
+import BWButton from '../Reusables/BWButton'
  
 
 const SelectedWorks = () => {
+    const [newZindex, setNewZindex] = useState(false)
+
+    const clickHandle = () => {
+        setNewZindex(true)
+    }
 
   return (
     <Box 
@@ -21,19 +27,25 @@ const SelectedWorks = () => {
     >
     <Box sx={{ display: 'flex', width: '100%', justifyContent: 'center'}}>
         <LeftSideContent >
-            <Box sx={{ position: 'absolute', marginLeft: '4vw'}}>
+            <Box>
                 <Typography variant='h1' sx={{ fontSize: '5vw', lineHeight: '100%'}}>SELECTED <br /> WORKS</Typography>
             </Box>
+            <Box sx={{ paddingTop: '10vh', paddingBottom: '10vh'}}>
+                <Button onClick={clickHandle}>Cycle Through Image</Button>
+            </Box>
+            <BWButton text='see all designs' />
         </LeftSideContent>
 
         <RightSideContent>
-            <Box sx={{ position: 'relative', zIndex: 1, top: '14vw', transform: 'translate(9vh, -5vw)', overflow: 'hidden'}}>
+            {newZindex ? (<Box sx={{ position: 'relative', zIndex: 5, top: '15vw', transform: 'translate(10vh, -5vw)', overflow: 'hidden'}}>
                 <Image src={Image3}/>
-            </Box>
-            <Box sx={{ position: 'relative', zIndex: 2, top: '17vw', transform: 'translate(6vh, -28vw)'}}>
+            </Box>) : (<Box sx={{ position: 'relative', zIndex: 1, top: '15vw', transform: 'translate(10vh, -5vw)', overflow: 'hidden'}}>
+                <Image src={Image3}/>
+            </Box>)}
+            <Box sx={{ position: 'relative', zIndex: 2, top: '18vw', transform: 'translate(6vh, -28vw)'}}>
                 <Image src={Image2}/>
             </Box>
-            <Box sx={{ position: 'relative', zIndex: 3, transform: 'translate(2vh, -28vw)'}}>
+            <Box sx={{ position: 'absolute', zIndex: 3, transform: 'translate(2vh, -28vw)'}}>
                 <Image src={Image1}/>
             </Box>
         </RightSideContent>
--- components/styles/SelectedWorksStyles.ts
+++ components/styles/SelectedWorksStyles.ts
@@ -2,11 +2,15 @@ import { styled } from '@mui/material/styles'
 import { Box } from '@mui/system'
 
 export const LeftSideContent = styled(Box)(() => ({
-    width: '50%'
+    width: '50%',
+    display: 'flex',
+    flexDirection: 'column',
+    marginLeft: '4vw',
 }))
 
 export const RightSideContent = styled(Box)(() => ({
     width: '50%',
     overflow: 'hidden',
     whiteSpace: 'nowrap',
+    position: 'relative',
 }))
