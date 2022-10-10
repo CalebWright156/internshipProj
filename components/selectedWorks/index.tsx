d7ffc963e035a46acd09a5728619cc98670c56f4
Caleb Wright <calebwright156@gmail.com>
2022-10-10 14:57:08 +1300

    finalised state changes

--- components/selectedWorks/index.tsx
+++ components/selectedWorks/index.tsx
@@ -10,16 +10,33 @@ import BWButton from '../Reusables/BWButton'
  
 
 const SelectedWorks = () => {
-    const [newZindex, setNewZindex] = useState(false)
 
-    const clickHandle = () => {
-        setNewZindex(true)
+    const [zImage3, setZImage3] = useState(false)
+    const [zImage2, setZImage2] = useState(false)
+    const [zImage1, setZImage1] = useState(false)
+
+    const image3Click = () => {
+        setZImage3(!zImage3)
+        setZImage2(false)
+        setZImage1(false)
+    }
+
+    const image2Click = () => {
+        setZImage2(!zImage2)
+        setZImage3(false)
+        setZImage1(false)
+    }
+
+    const image1Click = () => {
+        setZImage1(!zImage1)
+        setZImage2(false)
+        setZImage3(false)
     }
 
   return (
     <Box 
         sx={{
-            background: 'linear-gradient(180deg, rgba(14,18,25,1) 25%, rgba(67,65,73,0.5074404761904762) 100%)',
+            background: 'linear-gradient(180deg, hsl(240deg 8% 5%) 0%, hsl(233deg 7% 10%) 11%, hsl(228deg 9% 15%) 22%, hsl(224deg 10% 19%) 33%, hsl(220deg 12% 24%) 44%, hsl(217deg 13% 29%) 56%, hsl(214deg 14% 33%) 67%, hsl(211deg 16% 38%) 78%, hsl(209deg 17% 43%) 89%, hsl(207deg 18% 48%) 100%)',
             width: '100%',
             marginTop: '-15vh',
             zIndex: 1,
@@ -30,22 +47,22 @@ const SelectedWorks = () => {
             <Box>
                 <Typography variant='h1' sx={{ fontSize: '5vw', lineHeight: '100%'}}>SELECTED <br /> WORKS</Typography>
             </Box>
-            <Box sx={{ paddingTop: '10vh', paddingBottom: '10vh'}}>
-                <Button onClick={clickHandle}>Cycle Through Image</Button>
+            <Box sx={{ paddingTop: '10vh', paddingBottom: '10vh', display: 'flex', flexDirection: 'column', alignItems: 'start'}}>
+                <Button onClick={image3Click}>Case study 3</Button>
+                <Button onClick={image2Click}>Case study 2</Button>
+                <Button onClick={image1Click}>Case study 1</Button>
             </Box>
             <BWButton text='see all designs' />
         </LeftSideContent>
 
         <RightSideContent>
-            {newZindex ? (<Box sx={{ position: 'relative', zIndex: 5, top: '15vw', transform: 'translate(10vh, -5vw)', overflow: 'hidden'}}>
+           <Box sx={{ position: 'relative', zIndex: zImage3 ? 5 : 1, top: '15vw', transform: 'translate(10vh, -5vw)', overflow: 'hidden'}}>
                 <Image src={Image3}/>
-            </Box>) : (<Box sx={{ position: 'relative', zIndex: 1, top: '15vw', transform: 'translate(10vh, -5vw)', overflow: 'hidden'}}>
-                <Image src={Image3}/>
-            </Box>)}
-            <Box sx={{ position: 'relative', zIndex: 2, top: '18vw', transform: 'translate(6vh, -28vw)'}}>
+            </Box>
+            <Box sx={{ position: 'relative', zIndex: zImage2 ? 5 : 2, top: '18vw', transform: 'translate(6vh, -28vw)'}}>
                 <Image src={Image2}/>
             </Box>
-            <Box sx={{ position: 'absolute', zIndex: 3, transform: 'translate(2vh, -28vw)'}}>
+            <Box sx={{ position: 'absolute', zIndex: zImage1 ? 5: 3, transform: 'translate(2vh, -28vw)'}}>
                 <Image src={Image1}/>
             </Box>
         </RightSideContent>
